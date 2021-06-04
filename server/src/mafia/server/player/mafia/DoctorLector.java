8913508c58d5a6b0e29403af8c64553a875ac5d5
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-06-04 14:23:49 +0430

    Add javaDoc to DoctorLector

--- server/src/mafia/server/player/mafia/DoctorLector.java
+++ server/src/mafia/server/player/mafia/DoctorLector.java
@@ -7,13 +7,20 @@ import mafia.server.player.mafia.traits.CanSelectPlayerTrait;
 import mafia.server.state.GameState;
 import mafia.server.workers.PlayerWorker;
 
+/**
+ * The role DoctorLector in mafia game.
+ */
 public class DoctorLector extends Mafia implements CanSeeAllMafiasTrait, CanSelectPlayerTrait {
     private boolean hasCuredHimself = false;
 
+    /**
+     * Select mafia to cure.
+     *
+     * @param doctorLector the doctor lector
+     */
     public void selectMafiaToCure(PlayerWorker doctorLector) {
         this.showAllMafiasToClient(doctorLector);
         String cureTargetUsername = this.getPlayerUsername(doctorLector);
-
         Player cureTarget = GameState.getPlayerByUsername(cureTargetUsername).getPlayer();
 
         while (!cureTarget.isMafia()) {
@@ -22,6 +29,18 @@ public class DoctorLector extends Mafia implements CanSeeAllMafiasTrait, CanSele
             cureTarget = GameState.getPlayerByUsername(cureTargetUsername).getPlayer();
         }
 
-        cureTarget.revive();
+        while (doctorLector.getUsername().equals(cureTargetUsername) && this.hasCuredHimself) {
+//            doctor lector has cured himself once in the past can not cure himself again.
+            this.showAllMafiasToClient(doctorLector);
+            cureTargetUsername = this.getPlayerUsername(doctorLector);
+            cureTarget = GameState.getPlayerByUsername(cureTargetUsername).getPlayer();
+        }
+
+
+        if (doctorLector.getUsername().equals(cureTargetUsername)) {
+            this.hasCuredHimself = true;
+        } else {
+            cureTarget.revive();
+        }
     }
 }
