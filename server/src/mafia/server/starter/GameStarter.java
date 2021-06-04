1c1c6df011bc506e38e9cedf4327e5cc99c5a23c
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-06-04 13:12:14 +0430

    Add JavadocBlock comments to GameStarter

--- server/src/mafia/server/starter/GameStarter.java
+++ server/src/mafia/server/starter/GameStarter.java
@@ -12,12 +12,22 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * The starter of the game, which handles players being added to the server and starting the game loop.
+ * it also will set the GameState before the game start.
+ */
 public class GameStarter {
     private final ArrayList<PlayerWorker> players = new ArrayList<>();
     private final int maxPlayerCount = 10;
     private PlayerWorker serverAdmin = null;
 //    private GameState gameState;
 
+    /**
+     * Check if already have a worker with same username.
+     *
+     * @param username the username
+     * @return the boolean
+     */
     public boolean usernameExist(String username) {
         for (PlayerWorker player : players) {
             if (player.getUsername().equals(username)) {
@@ -28,10 +38,20 @@ public class GameStarter {
         return false;
     }
 
+    /**
+     * Is server full.
+     *
+     * @return the boolean
+     */
     public boolean isServerFull() {
         return this.players.size() == this.maxPlayerCount;
     }
 
+    /**
+     * Start new game by assigning roles to ever player and making a new instance of GameLoop.
+     *
+     * @throws NotEnoughPlayerException the not enough player exception
+     */
     public void startNewGame() throws NotEnoughPlayerException {
         if (!this.isServerFull()) {
             throw new NotEnoughPlayerException("server is not full");
@@ -47,6 +67,11 @@ public class GameStarter {
 
     }
 
+    /**
+     * Add player worker to the list of game players.
+     *
+     * @param playerWorker the player worker
+     */
     public void addPlayer(PlayerWorker playerWorker) {
         if (this.players.isEmpty()) {
             this.serverAdmin = playerWorker;
@@ -57,6 +82,11 @@ public class GameStarter {
         }
     }
 
+    /**
+     * Server admin wants previous game load.
+     *
+     * @return the boolean
+     */
     public boolean serverAdminWantsPreviousGameLoad() {
         try {
             Socket socket = this.serverAdmin.getSocket();
@@ -72,6 +102,9 @@ public class GameStarter {
         }
     }
 
+    /**
+     * Send other players message indicating that a new player has joined the game.
+     */
     public void sendPlayerConnectedMessageToOthers() {
         PlayerWorker lastAddedPlayer = this.players.get(this.players.size() - 1);
         for (PlayerWorker playerWorker : this.players) {
