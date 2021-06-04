72e27bf1bdee4e156d45ebedc2b2f273b59e9e1a
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-06-04 14:27:11 +0430

    Add javaDoc block to GodFather class

--- server/src/mafia/server/player/mafia/GodFather.java
+++ server/src/mafia/server/player/mafia/GodFather.java
@@ -2,10 +2,22 @@ package mafia.server.player.mafia;
 
 import mafia.server.player.mafia.abstacts.Mafia;
 
+/**
+ * The role God Father in mafia game
+ */
 public class GodFather extends Mafia {
+    /**
+     * Instantiates a new God father and set it to be the mafias leader.
+     */
     public GodFather() {
 //        god father is the leader of mafia's at the game start.
 //        he will be leader until he get killed.
         this.makeLeader();
+    }
+
+    @Override
+    public void kill() {
+        this.isLeader = false;
+        super.kill();
     }
 }
