64640933245a2a14ce5343c85ab74636cff969bf
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-06-04 14:24:49 +0430

    Add javaDoc to normal mafia

--- server/src/mafia/server/player/mafia/NormalMafia.java
+++ server/src/mafia/server/player/mafia/NormalMafia.java
@@ -3,5 +3,8 @@ package mafia.server.player.mafia;
 import mafia.server.player.mafia.abstacts.Mafia;
 import mafia.server.player.mafia.traits.CanVoteForKillTargetTrait;
 
+/**
+ * The role normal mafia in mafia game.
+ */
 public class NormalMafia extends Mafia implements CanVoteForKillTargetTrait {
 }
