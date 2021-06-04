7a9be2a67f2a0c284a02d0d7060e52696d332d18
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-06-04 13:00:19 +0430

    Finish player abstract class

--- server/src/mafia/server/player/Player.java
+++ server/src/mafia/server/player/Player.java
@@ -1,7 +1,26 @@
 package mafia.server.player;
 
 public abstract class Player {
+    private boolean speakPermission;
     private int id;
+    private boolean isKilled;
+    private boolean isServerAdmin;
+
+    public void setSpeakPermission(boolean speakPermission) {
+        this.speakPermission = speakPermission;
+    }
+
+    public boolean haveSpeakPermission() {
+        return speakPermission;
+    }
+
+    public boolean isServerAdmin() {
+        return this.isServerAdmin;
+    }
+
+    public void setServerAdmin(boolean serverAdmin) {
+        isServerAdmin = serverAdmin;
+    }
 
     public int getId() {
         return id;
@@ -9,5 +28,13 @@ public abstract class Player {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public boolean isKilled() {
+        return this.isKilled;
+    }
+
+    public void setKilled(boolean killed) {
+        isKilled = killed;
     }
 }
